d36a2f835dcd751ca13d55be74352506b4a06604
Pedro Rangel <pedronadu102@hotmail.com>
2018-04-06 00:48:46 -0300

    retirando const da tabela de simbolos

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -64,7 +64,6 @@ public class SymbolTable {
     final byte ACOLCHETES = 35;
     final byte FCOLCHETES = 36;
     final byte DO = 37;
-    final byte CONST = 38;
     //final byte EOF = 39;
 
     public SymbolTable() {
@@ -106,7 +105,6 @@ public class SymbolTable {
         table.put("[", new Symbol("[", ACOLCHETES, ++pos));
         table.put("]", new Symbol("]", FCOLCHETES, ++pos));
         table.put("do", new Symbol("do", DO, ++pos));
-        table.put("const", new Symbol("const", CONST, ++pos));
         //table.put("eof", new Symbol ( "eof" , EOF , ++pos ));
 
     }
